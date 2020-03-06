0a936b845b8f75325a0f6cf17cd283d5fc87fcd8
Alexander Clouter <alex@digriz.org.uk>
2020-03-06 08:56:04 +0000

    make partly implicit safer

--- webroot/oauth2-redirect.js
+++ webroot/oauth2-redirect.js
@@ -1,15 +1,36 @@
-function qs2o(s) {
-    s = s.split(/[#?]/)[1];
-    return s.split('&').reduce((a, p) => {
-        const m = p.split('=');
-        a[decodeURIComponent(m[0])] = decodeURIComponent(m[1]);
-        return a;
-    }, {});
-}
+(function(){
+  function qs2o(s) {
+      s = s.split(/[#?]/)[1];
+      return s.split('&').reduce((a, p) => {
+          const m = p.split('=');
+          a[decodeURIComponent(m[0])] = decodeURIComponent(m[1]);
+          return a;
+      }, {});
+  }
 
-const o = qs2o(location.hash.length ? location.hash : location.search);
+  const ls = location.search, lh = location.hash;
 
-if (o.state)
-    opener.postMessage({ id: o.state, ok: true, data: o })
-else
-    opener.postMessage({ id: null, ok: false, data: { error: o.error, description: o.error_description, location: { search: location.search, hash: location.hash } } });
+  location.hash = '';
+  history.replaceState(null, '', '/');
+
+  const o = qs2o(lh.length ? lh : ls);
+
+  const data = {
+    id: o.state || null,
+    ok: !!o.state,
+    data: o
+  };
+
+  if (!data.ok) {
+    data.data = {
+      error: o.error,
+      description: o.error_description,
+      location: {
+        search: location.search,
+        hash: location.hash
+      }
+    };
+  }
+
+  opener.postMessage(data);
+}).call(this);
